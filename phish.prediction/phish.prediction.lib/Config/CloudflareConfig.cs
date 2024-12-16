962a1ad9cb0c5fbcaf25014487dd63458b7f3882
Levi Hutchins <levihutchins0@gmail.com>
2024-12-16 22:59:14 +1100

    fix pipeline warnings

--- phish.prediction/phish.prediction.lib/Config/CloudflareConfig.cs
+++ phish.prediction/phish.prediction.lib/Config/CloudflareConfig.cs
@@ -2,6 +2,6 @@ namespace phish.prediction.lib.Features.Cloudflare.Config;
 
 public class CloudflareConfig
 {
-    public string ScanEndpoint { get; set; }
-    public string ApiKey { get; set; }
+    public required string ScanEndpoint { get; set; }
+    public required string ApiKey { get; set; }
 }
